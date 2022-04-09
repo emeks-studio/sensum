33cac704b3ea34df9e97ce85a50081fe5e42c9f3
tortugo <mirko@emeks.com.ar>
2022-04-09 18:20:43 -0300

    fix contract comments

--- sensum-ethereum/contracts/Sensations.sol
+++ sensum-ethereum/contracts/Sensations.sol
@@ -3,8 +3,8 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "hardhat/console.sol";
 /**
- * @title Storage
- * @dev Store & retrieve value in a variable
+ * @title Sensations
+ * @dev Store & retrieve sensum sensations
  */
 contract Sensations {
 

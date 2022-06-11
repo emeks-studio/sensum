ca1b424c1ac0b6d69d0faf2e2a9f736f12d1c1b6
ariedro <ariedro@gmail.com>
2022-06-11 04:49:42 -0300

    refactor: separate basic and legacy sensation contract tests

---
+++ sensum-ethereum/test/sensations-legacy-test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const legacySensations = require("./legacy-sensations.json");
+
+describe("legacy sensations contract test suite", function () {
+  let Sensations;
+  let sensations;
+  beforeEach(async function () {
+    Sensations = await ethers.getContractFactory("Sensations");
+    sensations = await Sensations.deploy();
+  });
+  it("Should be able to handle all the legacy sensations", async function () {
+    this.timeout(0); // will take a while
+    for (let i = 0; i < legacySensations.length; i++) {
+      const tx = await sensations.newSensation(legacySensations[i]);
+      const receipt = await tx.wait();
+      const eventArgs = receipt.events.find(({ event }) => event === "Synapsis").args[0];
+
+      expect(await sensations.getSensationsLength()).to.equal(i + 1);
+      expect(eventArgs[1]).to.equal(legacySensations[i].message);
+      expect(parseInt(eventArgs[0]._hex, 16)).to.equal(legacySensations[i].avatar);
+    }
+  });
+});
--- sensum-ethereum/test/sensations-test.js
+++ sensum-ethereum/test/sensations-test.js
@@ -1,17 +1,5 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const legacySensations = require("./legacy-sensations.json");
-
-const checkSensation = async (contract, sensation, expectedLength) => {
-  const tx = await contract.newSensation(sensation);
-  const receipt = await tx.wait();
-  const eventArgs = receipt.events.find(({ event }) => event === "Synapsis")
-    .args[0];
-
-  expect(await contract.getSensationsLength()).to.equal(expectedLength);
-  expect(eventArgs[1]).to.equal(sensation.message);
-  expect(parseInt(eventArgs[0]._hex, 16)).to.equal(sensation.avatar);
-};
 
 describe("sensations contract test suite", function () {
   let Sensations;
@@ -22,8 +10,16 @@ describe("sensations contract test suite", function () {
   });
   it("Should have 0 sensations in the begining", async () =>
     expect(await sensations.getSensationsLength()).to.equal(0));
-  it("Should be able to send a sensation", () =>
-    checkSensation(sensations, { avatar: 0, message: "M o n i" }, 1));
+  it("Should be able to send a sensation", async () => {
+    const sensation = { avatar: 0, message: "M o n i" };
+    const tx = await sensations.newSensation(sensation);
+    const receipt = await tx.wait();
+    const eventArgs = receipt.events.find(({ event }) => event === "Synapsis").args[0];
+
+    expect(await sensations.getSensationsLength()).to.equal(1);
+    expect(eventArgs[1]).to.equal(sensation.message);
+    expect(parseInt(eventArgs[0]._hex, 16)).to.equal(sensation.avatar);
+  });
   it("Should fail when sensation message is too large", async () => {
     await expect(
       sensations.newSensation({
@@ -31,10 +27,5 @@ describe("sensations contract test suite", function () {
         message: "x".repeat(1000),
       })
     ).to.be.revertedWith("too long sensation message");
-  });
-  it("Should be able to handle all the legacy sensations", async function () {
-    this.timeout(0); // will take a while
-    for (let i = 0; i < legacySensations.length; i++)
-      await checkSensation(sensations, legacySensations[i], i + 1);
   });
 });
